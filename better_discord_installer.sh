c01efa5e9d87635881461c0f0e732308328f8ae0
Lucas Morais <lucascdemorais@gmail.com>
2024-07-12 13:00:51 -0300

    replacing build to installer file

--- better_discord_installer.sh
+++ better_discord_installer.sh
@@ -1,31 +1,14 @@
 #!/bin/bash
 
 source check_internet.sh
-BETTER_DISCORD_PATH="$HOME/.better_discord/"
 CUR_PATH="$(pwd)"
 
-if ! command -v npm &>/dev/null; then
-	echo "Please certify that you have the npm installed before run this script."
-	exit 1
-fi
-
-if ! command -v pnpm &>/dev/null; then
-	echo "Please certify that you have the pnpm installed before run this script."
-	exit 1
-fi
+TEMP_P=/tmp/$(uuidgen)
+BETTER_DISCORD=$TEMP_P/better_discord.AppImage
 
 sudo killall Discord
-
-if [! -d $BETTER_DISCORD_PATH ]; then
-	git clone https://github.com/BetterDiscord/BetterDiscord.git $BETTER_DISCORD_PATH
-fi
-
-cd $BETTER_DISCORD_PATH
-git checkout main
-git pull
-
-pnpm install
-pnpm build
-pnpm inject
-
+wget https://github.com/BetterDiscord/Installer/releases/latest/download/BetterDiscord-Linux.AppImage -O $BETTER_DISCORD
+chmod +x $BETTER_DISCORD
+$BETTER_DISCORD
+rm -rf $TEMP_P
 cd $CUR_PATH
